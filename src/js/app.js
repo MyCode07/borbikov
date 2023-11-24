0afafe45a2e0355c836d3202a7d32361ee6f9eaa
MyCode07 <erem19992015@mail.ru>
2023-11-24 19:16:02 +0300

    header menu first commit

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,11 @@
 // import { accorden } from "./static/accordeon.js";
 // import { maskInputs } from "./static/inputmask.js";
 // import { runTicker } from "./static/ticker.js";
-// import { replaceDomElements } from "./static/replace.js";
+import { replaceDomElements } from "./static/replace.js";
 // import { toTop } from "./static/to-top.js";
 // import { stickyHeader } from "./parts/header.js";
 
-// import "./parts/menu.js";
+import "./parts/menu.js";
 // import "./parts/header.js";
 // import "./parts/animations.js";
 // import "./parts/mobile-controll.js";
@@ -16,7 +16,7 @@
 
 // stickyHeader()
 // toTop();
-// replaceDomElements();
+replaceDomElements();
 // runTicker()
 // accorden();
 // maskInputs('+7 999 999 999 999', '.phone')
--- src/js/parts/menu.js
+++ src/js/parts/menu.js
@@ -1,101 +1,104 @@
+import { isMobile } from '../utils/isMobile.js';
 import { lockPadding, unLockPadding } from '../utils/lockPadding.js';
 
 const body = document.body;
-const menuLinks = document.querySelectorAll('[data-open-menu]');
+const menu = document.querySelector('.header__bottom');
+const burger = document.querySelector('.header__burger');
+const menuLinks = document.querySelectorAll('.menu li a');
 const header = document.querySelector('.header');
-const menuAll = document.querySelectorAll('.menu');
 
-const menuItem = document.querySelectorAll('._menu-item');
 
-if (menuItem) {
-    menuItem.forEach(linkBcg => {
-        linkBcg.addEventListener('click', (e) => {
-            if (document.querySelector('._menu-item._active')) {
-                menuItem.forEach(link => {
-                    link.classList.remove('_active');
-                })
-                e.target.classList.toggle('_active');
-            }
-            else {
-                e.target.classList.toggle('_active');
-            }
-        })
+if (burger) {
+    burger.addEventListener('click', (е) => {
+        burger.classList.toggle('_active');
+        menu.classList.toggle('_open');
+        document.body.classList.toggle('_noscroll');
+
+
+        if (!header.classList.contains('_scrolled')) {
+            header.classList.toggle('_sticky');
+        }
+
+        if (menu.classList.contains('_open')) {
+            lockPadding();
+            setMenuTopPosition();
+        }
+        else {
+            unLockPadding()
+            resetMenuTopPosition();
+        }
     })
 }
 
-if (menuAll.length) {
-    const headerCoords = document.querySelector('.header').getBoundingClientRect();
-    menuAll.forEach(menu => {
-        if (document.querySelector('.offer._close')) {
-            menu.style.top = headerCoords.height + 40 + "px";
-        }
-        if (menu.dataset.id == 'menu-basket') {
-            menu.style.top = 0 + "px";
-        }
-        else {
-            menu.style.top = headerCoords.height + "px";
-        }
-    })
+function setMenuTopPosition() {
+    menu.style.top = header.getBoundingClientRect().height + 'px';
+}
+
+function resetMenuTopPosition() {
+    menu.style.top = 0;
 }
 
 if (menuLinks.length) {
     menuLinks.forEach(link => {
-        const menuType = link.dataset.openMenu
-        const menu = document.querySelector(`.menu[data-id="${menuType}"]`);
+        link.addEventListener('click', (е) => {
 
-        link.addEventListener('click', (e) => {
-            if (menu) {
+            if (!isMobile.any())
                 if (menu.classList.contains('_open')) unLockPadding();
                 else lockPadding()
 
-                if (document.querySelector('.menu._open')) {
-                    menuAll.forEach(menu => {
-                        menu.classList.remove('_open');
-                        body.classList.remove('_noscroll');
-                    })
-                    menu.classList.toggle('_open');
+            menu.classList.toggle('_open');
+            burger.classList.toggle('_active');
 
-                } else {
-                    menu.classList.toggle('_open');
-                }
-
-                header.classList.add('_active');
-                body.classList.toggle('_noscroll');
-            }
+            body.classList.toggle('_noscroll');
         })
     })
 }
 
-document.addEventListener('click', (e) => {
-    let targetEl = e.target;
+
+const arrow = `<button><svg xmlns="http://www.w3.org/2000/svg" width="9" height="6" viewBox="0 0 9 6" fill="none">
+<path d="M4.92922 5.33709L8.82427 1.79221C8.93758 1.6871 9 1.54677 9 1.39715C9 1.24753 8.93758 1.10721 8.82427 1.00209L8.46387 0.667391C8.22902 0.4496 7.84733 0.4496 7.61285 0.667391L4.50228 3.48384L1.38715 0.663156C1.27384 0.558038 1.1228 0.5 0.961732 0.5C0.800489 0.5 0.649442 0.558038 0.536044 0.663156L0.17573 0.997855C0.0624217 1.10306 -3.24905e-08 1.24329 -3.90307e-08 1.39292C-4.55708e-08 1.54254 0.0624217 1.68286 0.17573 1.78798L4.07525 5.33709C4.18892 5.44246 4.34068 5.50033 4.50201 5.5C4.66397 5.50033 4.81564 5.44246 4.92922 5.33709Z" fill="white"/>
+</svg></button>
+`;
+
+const submenuList = document.querySelectorAll('nav ul li');
+if (submenuList.length) {
+    submenuList.forEach(li => {
+        const submenu = li.querySelector('ul');
+
+        if (submenu) {
+            const link = submenu.previousElementSibling;
+            link.insertAdjacentHTML('afterend', arrow);
+
+            const btn = li.querySelector('button');
+
+            if (btn && isMobile.any()) {
+                btn.addEventListener('click', function () {
+                    toggleMenu(li)
+                })
+            }
+        }
+    })
 
 
-    if (!targetEl.hasAttribute('data-open-menu') && !targetEl.classList.contains('menu') && !targetEl.closest('.menu') && document.querySelector('.menu._open') && !targetEl.classList.contains('_close-button')) {
-        document.querySelector('.menu._open').classList.remove('_open')
-        document.querySelector('._menu-item._active').classList.remove('_active')
-        header.classList.remove('_active');
-        body.classList.remove('_noscroll');
+    function toggleMenu(item) {
+        const menu = item.closest('ul');
+        const menuItems = menu.querySelectorAll('li');
+
+        if (!item.hasAttribute('data-open')) {
+            const openitem = menu.querySelector('li[data-open]');
+            if (openitem) {
+                openitem.removeAttribute('data-open')
+            }
+
+            menuItems.forEach(item => {
+                item.removeAttribute('data-open')
+            })
+
+            item.setAttribute('data-open', 'open')
+        }
+        else {
+            item.removeAttribute('data-open')
+        }
     }
 
-    if (targetEl.classList.contains('_close-button')) {
-        const offer = targetEl.closest('.offer');
-        const menu = targetEl.closest('._basket')
-        const login = targetEl.closest('.login')
-
-        if (offer) {
-            offer.classList.add('_close')
-            header.classList.add('_top');
-            setTimeout(() => {
-                offer.remove()
-            }, 500);
-        }
-
-        if (document.querySelector('.menu._open')) {
-            menu.classList.remove('_open')
-        }
-
-        if (document.querySelector('.login._open')) {
-            login.classList.remove('_open')
-        }
-    }
-})
+}
