2a62c7d5c0535401a213cddf64d730cacec7d404
MyCode07 <erem19992015@mail.ru>
2023-12-06 13:10:26 +0300

    change text block height animation to gsap

--- src/js/parts/read-more.js
+++ src/js/parts/read-more.js
@@ -1,15 +1,32 @@
+import { gsap } from "gsap";
+
 document.addEventListener('click', function (e) {
     let targetEl = e.target;
 
     if (targetEl.classList.contains('_read-more')) {
         const showText = targetEl.dataset.showText;
 
-        const textContnet = targetEl.closest('.text-content')
+        const textContnet = targetEl.closest('.text-content').querySelector('.text-content__hidden')
         if (textContnet) {
             textContnet.classList.toggle('_active');
 
-            if (textContnet.classList.contains('_active')) targetEl.textContent = 'Скрыть';
-            else targetEl.textContent = showText;
+            if (textContnet.classList.contains('_active')) {
+                targetEl.textContent = 'Скрыть';
+
+                gsap.to(textContnet, {
+                    height: 'auto',
+                    duration: 0.5
+                })
+
+            }
+            else {
+                targetEl.textContent = showText;
+
+                gsap.to(textContnet, {
+                    height: 292,
+                    duration: 0.5
+                })
+            }
         }
     }
 
